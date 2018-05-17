d258869a9ee3a59ad5a6d19b8672fcc49b75756f
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-05-16 17:29:03 -0700

    fixed styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import marked from 'marked';
+import './index.css'
 
 class DisplayContainer extends Component {
     constructor() {
@@ -20,7 +21,7 @@ class DisplayContainer extends Component {
     render() {
         return (
             <div>
-                <header>React Markdown</header>
+                <header className="Index-header">React Markdown</header>
                 <RawInput value={this.state.value} updateValue={this.updateValue}/>
                 <span dangerouslySetInnerHTML={this.convertMarkdown()}></span>
             </div>
@@ -30,7 +31,7 @@ class DisplayContainer extends Component {
 const RawInput = ({value, updateValue}) => {
     return (
         <div>
-            <textarea rows="4" cols="50" onChange={updateValue} value={value}>
+            <textarea className="Type-here" rows="8" cols="145" onChange={updateValue} value={value}>
             </textarea>
         </div>
     )
