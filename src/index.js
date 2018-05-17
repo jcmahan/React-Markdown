d0d0de631d0cbb83240145388a92d3c314b86d7c
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-05-16 17:01:35 -0700

    fixed functionality, waiting on styling.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import marked from 'marked';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class DisplayContainer extends Component {
+    constructor() {
+    super();
+    this.state = {
+        value: ""
+    }
+}
+    updateValue = (e) => {
+        this.setState({value:e.target.value})
+        console.log(this.convertMarkdown());
+    }
+    convertMarkdown = () => {
+        return {__html: marked(this.state.value)}
+    }
+
+    render() {
+        return (
+            <div>
+                <header>React Markdown</header>
+                <RawInput value={this.state.value} updateValue={this.updateValue}/>
+                <span dangerouslySetInnerHTML={this.convertMarkdown()}></span>
+            </div>
+        )}
+}
+
+const RawInput = ({value, updateValue}) => {
+    return (
+        <div>
+            <textarea rows="4" cols="50" onChange={updateValue} value={value}>
+            </textarea>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<DisplayContainer />, document.getElementById('root'))
